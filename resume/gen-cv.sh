f424a5615a2c2aa403d88fb885b1939069554ee5
Matthieu Basset <bassetmatt@gmail.com>
2025-08-09 16:08:48 +0200

    Update script and commit pdfs

--- resume/gen-cv.sh
+++ resume/gen-cv.sh
@@ -2,3 +2,9 @@ echo "#let DARK = true" > lib/config.typ
 typst c CV-EN.typ CV-EN-dark.svg
 echo "#let DARK = false" > lib/config.typ
 typst c CV-EN.typ CV-EN-light.svg
+typst c CV-EN.typ CV-EN.pdf
+typst c CV-FR.typ CV-FR.pdf
+
+typst c theme-dnd/CV-EN.typ theme-dnd/CV-EN.pdf --root .
+typst c theme-dnd/CV-EN.typ theme-dnd/CV-EN.svg --root .
+typst c theme-dnd/CV-FR.typ theme-dnd/CV-FR.pdf --root .
